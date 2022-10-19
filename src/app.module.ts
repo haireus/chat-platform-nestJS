74788f9b660b46b529449c257e64efcdd051ad9a
Hai. Dinh Van - CMC Global DU1.3 <dvhai2@cmcglobal.vn>
2022-10-19 11:09:01 +0700

    Impliment mysql and user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
+import { UserModule } from './user/user.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import entities from './utils/typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
     AuthModule,
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_DB_HOST,
+      port: process.env.MYSQL_DB_PORT,
+      username: process.env.MYSQL_DB_USERNAME,
+      password: process.env.MYSQL_DB_PASSWORD,
+      database: process.env.MYSQL_DB_DATABASE,
+      entities,
+      synchronize: true,
+    }),
   ],
   controllers: [],
   providers: [],
